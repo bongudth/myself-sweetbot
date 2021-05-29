bd5968fd01f2562ec3a2dc56c2705560ac44e100
HuynhThiKhanhLinh <bongudth@gmail.com>
2021-05-29 10:52:25 +0700

    Add webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 import express from "express";
+import chatbotController from "../controllers/chatbotController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    res.send("Hello World");
-  })
+  router.get("/", chatbotController.getHomePage);
+
+  router.get("/webhook", chatbotController.getWebhook);
+  router.post("/webhook", chatbotController.postWebhook);
 
   return app.use("/", router);
 };
